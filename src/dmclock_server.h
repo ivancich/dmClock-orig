e4f1ef71088ceb723b215a5f949f7ee6aa11d798
J. Eric Ivancich <ivancich@redhat.com>
2016-12-21 13:11:32 -0500

    TEMPORARY Add buffers around heap index data to see if other code might be clobbering heap index data inadvertantly.
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- src/dmclock_server.h
+++ src/dmclock_server.h
@@ -59,6 +59,12 @@
 #include <sstream>
 #endif
 
+#define CLOBBER_CHECK 1
+
+#ifdef CLOBBER_CHECK
+#warning clobber check turned on
+#endif
+
 #define CHECK 1
 
 #ifdef CHECK
@@ -304,11 +310,26 @@ namespace crimson {
 	// an idle client becoming unidle
 	double                prop_delta = 0.0;
 
+#ifdef CLOBBER_CHECK
+	char buff1[4] = { '\x0aa', '\x0aa', '\x0aa', '\x0aa' };
+#endif
 	c::IndIntruHeapData   reserv_heap_data;
+#ifdef CLOBBER_CHECK
+	char buff2[4] = { '\x0aa', '\x0aa', '\x0aa', '\x0aa' };
+#endif
 	c::IndIntruHeapData   lim_heap_data;
+#ifdef CLOBBER_CHECK
+	char buff3[4] = { '\x0aa', '\x0aa', '\x0aa', '\x0aa' };
+#endif
 	c::IndIntruHeapData   ready_heap_data;
 #if USE_PROP_HEAP
+#ifdef CLOBBER_CHECK
+	char buff4[4] = { '\x0aa', '\x0aa', '\x0aa', '\x0aa' };
+#endif
 	c::IndIntruHeapData   prop_heap_data;
+#endif
+#ifdef CLOBBER_CHECK
+	char buff5[4] = { '\x0aa', '\x0aa', '\x0aa', '\x0aa' };
 #endif
 
       public:
